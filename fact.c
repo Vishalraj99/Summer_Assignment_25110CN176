d1828ab073051da524b3b33a8022a2a5daa3b032
Vishal Raj <vishalraj12122006@gmail.com>
2026-06-01 11:19:28 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n,i,fact=1;
+    printf("enter the number");
+    scanf("%d",&n);
+if(n<0)
+{
+    printf("factorial of the given number is not defined");
+}
+else if(n==0||n==1)
+{
+    printf("factorial is 1");
+}
+else{
+    for(i=1;i<=n;i++)
+{
+    fact=fact*i; }
+    printf("factoral is %d",fact);
+}
+return 0;
+}
