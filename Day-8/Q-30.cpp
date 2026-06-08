6737ee5c7b32903b6e09651c144330b37c5bcb02
Vishal Raj <vishalraj12122006@gmail.com>
2026-06-08 09:21:46 +0530

    Day-8 uploaded

---
+++ Day-8/Q-30.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n,j;
+    cout<<"enter the rows";
+    cin>>n;
+    //outer loop for rows
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++)//for stars
+        {
+            cout<<j;
+        }
+       cout<<endl;
+    }
+  return 0;
+}
