159a93cb1983c09bb3a24a04119c2ee36c8b24f8
Vishal Raj <vishalraj12122006@gmail.com>
2026-06-22 19:03:16 +0530

    Day-22 uploaded

---
+++ Day-22/Q-85.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+ #include<cstring>
+ using namespace std;
+ int main()
+ {
+     char str[100], rev[100];
+     cout<<"Enter string: ";
+     cin>>str;
+     strcpy(rev,str);
+     strrev(rev);
+     if(strcmp(str,rev)==0)
+         cout<<"Palindrome";
+     else
+         cout<<"Not Palindrome";
+     return 0;
+ }
